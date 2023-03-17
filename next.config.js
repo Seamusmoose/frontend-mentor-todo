05c35403de027bf5d41124c7992ff33e0de8935f
Seamus OSullivan <info@seamuso.com>
2023-03-17 16:15:08 +0100

    svg issue fixed

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
-
-/**
- * @type {import('next-react-svg').NextReactSvgConfig}
- */
-const nextReactSvgConfig = {
-  include: path.resolve(__dirname, "src/assets/svg"),
-};
-
-/**
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
-  // ...
-};
+  reactStrictMode: true,
+}
 
-const withReactSvg = require("next-react-svg")(nextReactSvgConfig);
-
-module.exports = withReactSvg(nextConfig);
+module.exports = nextConfig
